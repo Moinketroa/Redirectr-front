bf8d14a03b15ca3c9aa8b6f59f68675b1a873323
Moinketroa <jean-marc.debicki1@etu.univ-lorraine.fr>
2017-11-30 14:56:24 +0100

    router + debut toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Material Design Imports
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
+import { APP_ROUTES } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    APP_ROUTES
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
