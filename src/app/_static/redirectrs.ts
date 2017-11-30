c75380a9aa5342dabb1e2d87566c80af0e7c390a
Moinketroa <jean-marc.debicki1@etu.univ-lorraine.fr>
2017-11-30 19:44:50 +0100

    ajout de données bidons pour test du front-end sans api

---
+++ src/app/_static/redirectrs.ts
@@ -0,0 +1,46 @@
+export const REDIRECTRS = [
+  {
+    'id': 12345,
+    'title': 'You reposted in the wrong neighborhood',
+    'description': 'Mashup Glue70 & Shake That-Nate Dogg',
+    'clicks': 12,
+    'main_link': 0,
+    'links': [
+      'https://www.youtube.com/watch?v=4feUSTS21-8',
+      'https://www.youtube.com/watch?v=pYHh-k8mqAs',
+      'https://www.youtube.com/watch?v=4MPw4vlQt0Y',
+      'https://www.youtube.com/watch?v=IdzI3tmvrCc'
+    ]
+  },
+  {
+    'id': 54321,
+    'title': 'Definition de confiture',
+    'description': 'Description du mot confiture',
+    'clicks': 52,
+    'main_link': 0,
+    'links': [
+      'http://www.larousse.fr/dictionnaires/francais/confiture/18121',
+      'http://www.linternaute.com/dictionnaire/fr/definition/confiture/',
+      'http://dictionnaire.reverso.net/francais-definition/confiture',
+      'https://fr.wikipedia.org/wiki/Confiture'
+    ]
+  },
+  {
+    'id': 67890,
+    'title': 'Never Gonna Shoot Your Stars',
+    'description': 'Never Gonna Give You Up - Shooting Stars Mashup by Grandayy',
+    'clicks': 12000,
+    'main_link': 0,
+    'links': [
+      'https://www.youtube.com/watch?v=Gc2u6AFImn8',
+      'https://soundcloud.com/grande1899/rick-astley-never-gonna-shoot-your-stars',
+      'https://coub.com/view/seqr7'
+    ]
+  },
+  {
+    'id': 09876,
+    'title': 'Empty Example',
+    'description': 'There is no link here :/',
+    'clicks': 3
+  }
+]
