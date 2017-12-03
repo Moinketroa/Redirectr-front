472dc86163bf0e04f69c8dcb06a1f26208a4c3f1
laurentstocchi <ls.laurentstocchi@gmail.com>
2017-12-03 03:00:32 +0100

    add redirectr step 2 : avec control de la saisie

--- src/app/add-redirectr/add-redirectr.component.ts
+++ src/app/add-redirectr/add-redirectr.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder,
+    Validators,ReactiveFormsModule, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'redirectr-add-redirectr',
   templateUrl: './add-redirectr.component.html',
   styleUrls: ['./add-redirectr.component.css']
 })
-export class AddRedirectrComponent implements OnInit {
+export class AddRedirectrComponent implements OnInit, OnChanges {
+    // private property to store model value
+    private _model: any;
+    // private property to store submit$ value
+    private _submit$: EventEmitter<any>;
+    // private property to store form value
+    private _form: FormGroup;
 
-  constructor() { }
+    /**
+     * Component constructor
+     */
+    constructor() {
+        this._submit$ = new EventEmitter();
+        this._form = this._buildForm();
+    }
 
-  ngOnInit() {
-  }
 
+    /**
+     * Returns private property _model
+     *
+     * @returns {any}
+     */
+    get model(): any {
+        return this._model;
+    }
+
+    /**
+     * Returns private property _form
+     *
+     * @returns {FormGroup}
+     */
+    get form(): FormGroup {
+        return this._form;
+    }
+
+    /**
+     * Returns private property _submit$
+     *
+     * @returns {EventEmitter<any>}
+     */
+    @Output('submit')
+    get submit$(): EventEmitter<any> {
+        return this._submit$;
+    }
+
+    /**
+     * OnInit implementation
+     */
+    ngOnInit() {
+    }
+
+    /**
+     * Function to handle component update
+     *
+     * @param record
+     */
+    ngOnChanges(record) {
+
+    }
+
+    /**
+     * Function to emit event to submit form and person
+     */
+    submit(person: any) {
+        this._submit$.emit(person);
+    }
+
+    /**
+     * Function to build our form
+     *
+     * @returns {FormGroup}
+     *
+     * @private
+     */
+    private _buildForm(): FormGroup {
+        return new FormGroup({
+            id: new FormControl(''),
+            title: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            description: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            main_link: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ]))
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Material Design Imports
 import { MatIconModule, MatListModule, MatToolbarModule } from '@angular/material';
@@ -34,6 +34,8 @@ import { AddRedirectrComponent } from './add-redirectr/add-redirectr.component';
     MatListModule,
     MatIconModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     APP_ROUTES
   ],
   providers: [RedirectrService],
